ea815204321a3e77ff4ee4f8587081e8f6138336
Zhyzhang1998 <93493602+Zhyzhang1998@users.noreply.github.com>
2022-12-11 20:10:20 -0500

    Data checkpoint

--- final.py
+++ final.py
@@ -2,6 +2,8 @@ import json
 import requests
 from requests_oauthlib import OAuth1
 import secret
+import webbrowser
+import yaml
 
 # Secret
 client_key = secret.TWITTER_API_KEY
@@ -23,7 +25,17 @@ endpoint_url_newsapi = 'https://newsapi.org/v2/everything'
 CACHE_FILENAME = "cache.json"
 
 def open_cache():
+    """Open saved cache or create a new cache.
 
+    Parameters
+    ----------
+    None
+
+    Returns
+    ----------
+    Dict
+        If there is not saved cache.
+    """
     try:
         with open(CACHE_FILENAME, 'r', encoding='utf-8') as file_obj:
             cache_dict = json.load(file_obj)
@@ -33,11 +45,33 @@ def open_cache():
 
 
 def save_cache(cache_dict, encoding='utf-8', ensure_ascii=False, indent=2):
+    """Save the cache file.
+
+    Parameters
+    ----------
+    cache_dict: Dict
+
+    Returns
+    ----------
+    None
+    """
     with open(CACHE_FILENAME, 'w', encoding=encoding) as file_obj:
         json.dump(cache_dict, file_obj, ensure_ascii=ensure_ascii, indent=indent)
 
 
 def get_data(keyword, platform):
+    """Get the data.
+
+    Parameters
+    ----------
+    keyword: str
+    platform: str
+
+    Returns
+    ----------
+    Dict
+        Json data.
+    """
     if platform == "twitter":
         params = {'q': keyword}
         return requests.get(endpoint_url_twitter, params=params, auth=oauth).json()
@@ -47,28 +81,67 @@ def get_data(keyword, platform):
 
 
 def write_json(filepath, data, encoding='utf-8', ensure_ascii=False, indent=2):
-    """Serializes object as JSON. Writes content to the provided filepath.
+    """Export the json data to files.
 
-    Parameters:
-        filepath (str): the path to the file
-        data (dict)/(list): the data to be encoded as JSON and written to the file
-        encoding (str): name of encoding used to encode the file
-        ensure_ascii (str): if False non-ASCII characters are printed as is;
-                            otherwise non-ASCII characters are escaped.
-        indent (int): number of "pretty printed" indention spaces applied to
-                      encoded JSON
+    Parameters
+    ----------
+    filepath: str
+    data: dict
 
-    Returns:
-        None
+    Returns
+    ----------
+    None
     """
 
     with open(filepath, 'w', encoding=encoding) as file_obj:
         json.dump(data, file_obj, ensure_ascii=ensure_ascii, indent=indent)
 
+def find_commonwords():
+    pass
+
+def generate_wordcloud():
+    pass
+
+
+
+# class Tweet:
+#     def __init__(self, dic):
+#         self.text = dic
+
+# class News:
+#     def __init__(self, dic):
+#         self.
 
 def main():
-    cache = open_cache()
+    """Print messages and start the program.
 
+    Parameters
+    ----------
+    None
+
+    Returns
+    ----------
+    None
+    """
+    print('Hello!')
+    cache = open_cache()
+    city = input('Which city do you want to know?\n')
+    keywords = input('What do you want to know? Give me one keyword.\n')
+    answer = city + " " +keywords
+
+    try:
+        # While we had the data cached before.
+        twitter_result = cache[city][keywords]['Twitter']
+        news_result = cache[city][keywords]['NewsAPI']
+
+    except:
+        # While there is no cache.
+        twitter_result = get_data(answer, "twitter")["statuses"]
+        news_result = get_data(answer, "newsapi")["articles"]
+        if city not in cache.keys(): # If the city not in cache
+            cache[city] = {keywords: {'Twitter': twitter_result, 'NewsAPI': news_result}}
+        elif keywords not in cache[city].keys(): # If the keywords not in cache
+            cache[city][keywords] = {'Twitter': twitter_result, 'NewsAPI': news_result}
 
 
     save_cache(cache)
